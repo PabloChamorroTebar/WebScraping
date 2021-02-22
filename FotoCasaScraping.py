e5f60a534744025752fdbd39f2d36455ef8ad7d2
Pablo Chamorro <pablochamorro@MacBook-Pro-de-Pablo.local>
2021-02-22 23:32:14 +0100

    First stable version with FotoCasa Scraping

---
+++ FotoCasaScraping.py
@@ -0,0 +1,148 @@
+import argparse
+import json
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+#from pwn import *
+
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+print(
+"-------------------------------------------------------------------------------------------------------------------------------------\n"
+"███████╗ ██████╗ ████████╗ ██████╗  ██████╗ █████╗ ███████╗ █████╗     ███████╗ ██████╗██████╗  █████╗ ██████╗ ██╗███╗   ██╗ ██████╗\n"+
+"██╔════╝██╔═══██╗╚══██╔══╝██╔═══██╗██╔════╝██╔══██╗██╔════╝██╔══██╗    ██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝\n"+
+"█████╗  ██║   ██║   ██║   ██║   ██║██║     ███████║███████╗███████║    ███████╗██║     ██████╔╝███████║██████╔╝██║██╔██╗ ██║██║  ███╗\n"+
+"██╔══╝  ██║   ██║   ██║   ██║   ██║██║     ██╔══██║╚════██║██╔══██║    ╚════██║██║     ██╔══██╗██╔══██║██╔═══╝ ██║██║╚██╗██║██║   ██║\n"+
+"██║     ╚██████╔╝   ██║   ╚██████╔╝╚██████╗██║  ██║███████║██║  ██║    ███████║╚██████╗██║  ██║██║  ██║██║     ██║██║ ╚████║╚██████╔╝\n"+
+"╚═╝      ╚═════╝    ╚═╝    ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝\n"
+"-------------------------------------------------------------------------------------------------------------------------------------\n")
+
+# Global variables
+path = "/Users/pablochamorro/Downloads/chromedriver"
+driver = webdriver.Chrome(path)
+fotocasa_base_path = "https://www.fotocasa.es"
+outputFile = None
+proxy = None
+
+
+def initVariables(args):
+
+    # Relleno de variables
+    url = args.url
+    outputFile = args.output
+    proxy = args.proxy
+
+    return url
+
+
+def obtainDatahome(link):
+
+    data_home = dict()
+    url = fotocasa_base_path + link
+    driver.get(url)
+    time.sleep(0.5)
+    htmlText = driver.page_source
+    soup = BeautifulSoup(htmlText, 'html.parser')
+    price = soup.find("span", class_="re-DetailHeader-price").text
+    rooms = soup.find_all("li", class_="re-DetailHeader-featuresItem")[0].find("span", class_=False).find(
+        "span").text
+    bathrooms = soup.find_all("li", class_="re-DetailHeader-featuresItem")[1].find("span", class_=False).find(
+        "span").text
+    house_size = soup.find_all("li", class_="re-DetailHeader-featuresItem")[2].find("span", class_=False).find(
+        "span").text
+    data_home["price"] = price
+    data_home["rooms"] = rooms
+    data_home["bathrooms"] = bathrooms
+    data_home["house_size"] = house_size
+    for feature in soup.find_all("div", class_="re-DetailFeaturesList-featureContent"):
+        data_home[feature.find("p", class_="re-DetailFeaturesList-featureLabel").text] = feature.find("p",
+                                                                                                      class_="re-DetailFeaturesList-featureValue").text
+
+    for extraFeature in soup.find_all("li", class_="re-DetailExtras-listItem"):
+        data_home[extraFeature.text] = True
+
+    return data_home
+
+def writeDataToFile(data_homes):
+
+    #p = log.progress("Data")
+
+    #p.status("Inserting data into %s file" % outputFile)
+
+    with open('data.txt', 'w') as outfile:
+        json.dump(data_homes, outfile)
+
+    #p.success("Insert data into %s file" % outputFile)
+
+def obtainLinks(url):
+
+    driver.get(url)
+    time.sleep(3)
+
+    try:
+        driver.find_element_by_css_selector("*[data-testid='TcfAccept']").click()
+    except:
+        print("Button not found")
+        driver.find_element_by_css_selector("*[data-testid='TcfAccept']")
+
+    listaEnlaces = set()
+
+    # Obtenemos todas las viviendas
+    #p = log.progress("Homes")
+    #p.status("Getting home links")
+    time.sleep(0.5)
+
+    for i in range(1, 20):
+        htmlText = driver.page_source
+        soup = BeautifulSoup(htmlText, 'html.parser')
+        viviendas = soup.find_all('a', class_="re-Card-link")
+        for vivienda in viviendas:
+            if vivienda['href'] and vivienda["href"].startswith("/es/"):
+                listaEnlaces.add(vivienda['href'])
+        ActionChains(driver).key_down(Keys.PAGE_DOWN).key_up(Keys.PAGE_DOWN).perform()
+        time.sleep(0.5)
+
+    #p.success("%d links obtained", len(listaEnlaces))
+
+    return listaEnlaces
+
+def obtainDataHomes(listaEnlaces):
+
+    #p = log.progress("Home")
+    counter = 1
+    data_homes = dict()
+    for link in listaEnlaces:
+        #p.status("Obtain vivienda [%d] data" % counter)
+        data_homes[counter] = obtainDatahome(link)
+        counter += 1
+
+    #p.success("Homes data collected")
+
+    return data_homes
+
+    p = log.progress("Data")
+
+    p.status("Inserting data into %s file" % outputFile)
+
+
+def main(args):
+
+    url = initVariables(args)
+    listaEnlaces = obtainLinks(url)
+    data_homes = obtainDataHomes(listaEnlaces)
+    driver.close()
+    writeDataToFile(data_homes)
+    time.sleep(2)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Scraping fotocasa web.')
+    parser.add_argument('url', type=str, help='Enter the url with filters apply')
+    parser.add_argument('--output', type=str, help="file with saved data (data.json by default)")
+    parser.add_argument('--proxy', type=str, help="proxy to jump blocking")
+
+    args = parser.parse_args()
+
+    main(args)
